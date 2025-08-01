9fcaa59e06a2d3bb218ec498e64e86da9cbd1a3e
Francisco Mondaca <mondaca@uni-koeln.de>
2025-08-01 20:09:48 +0200

    fix: import upload_status_view in views init

--- arkumu/storage/views/__init__.py
+++ arkumu/storage/views/__init__.py
@@ -3,5 +3,6 @@ from .dashboard_views import *
 from .direct_upload_views import *
 # from .presigned_url_views import * # Removed as per user request
 from .file_operations_views import *
+from .upload_status_view import *
 
 # This file marks the directory as a Python package
