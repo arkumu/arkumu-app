3d1b894876d6dca7acd0b06b63db4d399748b7fc
s6nistam <tstamos@uni-koeln.de>
2025-09-16 09:12:15 +0200

    fixed error that happened in renaming

--- arkumu/catalog/urls.py
+++ arkumu/catalog/urls.py
@@ -8,7 +8,7 @@ from .views import (
     HarmonizedResourceDetailView,
     HarmonizedSearchView,
     LiveSearchFilterView,
-    DesignSearch,
+    Card,
     ProjektShow
 )
 from .views_explorer import (
