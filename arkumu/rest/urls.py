5a80f201adac09269a5ef2383ac37dd240fbda6f
Francisco Mondaca <mondaca@uni-koeln.de>
2025-10-05 17:05:41 +0200

    rest: drop unused upload viewset

--- arkumu/rest/urls.py
+++ arkumu/rest/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from arkumu.rest.views.import_viewsets import ImportViewSet
-from arkumu.rest.views.upload_viewsets import UploadViewSet
 from arkumu.rest.views.canonical_uri_viewsets import CanonicalUriMappingViewSet
 
 app_name = 'rest'
@@ -14,8 +13,6 @@ router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 # Register ViewSets
 if getattr(settings, 'ENABLE_IMPORT_API', False):
     router.register(r'import', ImportViewSet, basename='import')
-
-router.register(r'upload', UploadViewSet, basename='upload')
 
 if getattr(settings, 'ENABLE_CANONICAL_URI_API', False):
     router.register(r'canonical-uri', CanonicalUriMappingViewSet, basename='canonical-uri')
