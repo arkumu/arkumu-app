cd6fd4dace233384bdf3f5cd1974aa4655f9ee09
Francisco Mondaca <mondaca@uni-koeln.de>
2025-11-13 18:00:04 +0100

    fix: expose impressum without login

--- arkumu/catalog/urls.py
+++ arkumu/catalog/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('components/', login_required(TemplateView.as_view(template_name="catalog/components.html")), name='components'),
     path('university/', login_required(TemplateView.as_view(template_name="catalog/university_page.html")), name='university_page'),
     path('documentation/', login_required(TemplateView.as_view(template_name="catalog/documentation.html")), name='documentation'),
-    path('impressum/', login_required(TemplateView.as_view(template_name="catalog/impressum.html")), name='impressum'),
+    path('impressum/', TemplateView.as_view(template_name="catalog/impressum.html"), name='impressum'),
     path('datenschutz/', login_required(TemplateView.as_view(template_name="catalog/datenschutz.html")), name='datenschutz'),
     path('university_page_FUK/', login_required(TemplateView.as_view(template_name="catalog/university_pages/university_page_FUK.html")), name='university_page_FUK'),
     path('university_page_RSH/', login_required(TemplateView.as_view(template_name="catalog/university_pages/university_page_RSH.html")), name='university_page_RSH'),
