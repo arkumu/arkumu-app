ff4aba2846b19aba8e4a5dd8df5e063b5d82e785
Francisco Mondaca <mondaca@uni-koeln.de>
2025-11-13 21:00:19 +0100

    fix: revert db dashboard link namespace

--- arkumu/oaipmh/dashboard_views.py
+++ arkumu/oaipmh/dashboard_views.py
@@ -276,7 +276,7 @@ def oai_db_dashboard(request):
         logger.exception("Failed to build DB OAI dashboard snapshot: %s", exc)
         oai_snapshot = None
 
-    db_endpoint_url = request.build_absolute_uri(reverse('oai:db-endpoint'))
+    db_endpoint_url = request.build_absolute_uri(reverse('oai_admin:db-endpoint'))
     snapshot_endpoint_url = request.build_absolute_uri(reverse('metadata:oai_proxy'))
     endpoints = _build_oai_endpoint_summaries(oai_snapshot)
 
