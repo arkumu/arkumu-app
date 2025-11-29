458dd55c859df8d3f66cb6fb8aa189d593ce04ac
Francisco Mondaca <mondaca@uni-koeln.de>
2025-11-29 16:09:44 +0100

    perf: add browser caching headers to preview images (24h cache + ETag)

--- arkumu/catalog/views/images.py
+++ arkumu/catalog/views/images.py
@@ -1,14 +1,18 @@
+import hashlib
+
 from django.http import HttpResponseNotFound, HttpResponse
-from django.utils.html import format_html
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from arkumu.catalog.models import PreviewImages
 
-import base64
-
 
 class ImagesView(LoginRequiredMixin, View):
+    """Serve preview images with browser caching."""
+
+    # Cache for 24 hours (browser won't hit DB again)
+    CACHE_MAX_AGE = 86400
+
     def get(self, request, *args, **kwargs):
         query = request.GET.get('query', '').strip()
         img = PreviewImages.objects.filter(path=query).first()
@@ -16,7 +20,17 @@ class ImagesView(LoginRequiredMixin, View):
         if not img:
             return HttpResponseNotFound("Image not found")
 
-        # Gib das reine Bild zurück mit korrektem Content-Type
-        return HttpResponse(img.img, content_type=img.content_type)
+        # Generate ETag from content hash
+        etag = hashlib.md5(img.img).hexdigest()
+
+        # Check If-None-Match header (browser cache validation)
+        if request.META.get('HTTP_IF_NONE_MATCH') == etag:
+            return HttpResponse(status=304)  # Not Modified
+
+        response = HttpResponse(img.img, content_type=img.content_type)
+        response['Content-Length'] = img.content_length
+        response['Cache-Control'] = f'private, max-age={self.CACHE_MAX_AGE}'
+        response['ETag'] = etag
+        return response
 
 
