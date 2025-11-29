9b82c5ac65cd8a6e1786671b93c54074034597da
Francisco Mondaca <mondaca@uni-koeln.de>
2025-11-29 16:14:56 +0100

    feat: add --backfill-etags flag to download_preview_imgs command

--- arkumu/catalog/management/commands/download_preview_imgs.py
+++ arkumu/catalog/management/commands/download_preview_imgs.py
@@ -1,5 +1,8 @@
+import hashlib
+
 from django.core.management import BaseCommand
 
+from arkumu.catalog.models import PreviewImages
 from arkumu.catalog.services.download_previews import DownloadPreviews
 
 
@@ -9,6 +12,26 @@ class Command(BaseCommand):
             '--force',
             action='store_true',
             help='Force download')
+        parser.add_argument(
+            '--backfill-etags',
+            action='store_true',
+            help='Backfill ETags for existing images without re-downloading')
 
     def handle(self, *args, **options):
-        DownloadPreviews().download_previews(options['force'])
+        if options['backfill_etags']:
+            self._backfill_etags()
+        else:
+            DownloadPreviews().download_previews(options['force'])
+
+    def _backfill_etags(self):
+        qs = PreviewImages.objects.filter(etag='')
+        total = qs.count()
+        self.stdout.write(f'Found {total} images without ETag')
+
+        updated = 0
+        for img in qs.iterator():
+            img.etag = hashlib.md5(img.img).hexdigest()
+            img.save(update_fields=['etag'])
+            updated += 1
+
+        self.stdout.write(self.style.SUCCESS(f'Updated {updated} images'))
