fd95ec522cb02e2cdef9983cf029f9d36ba9e372
Francisco Mondaca <mondaca@uni-koeln.de>
2025-12-10 10:53:28 +0100

    fix: update OAI_ROSETTA_CURATED_PREFIXES for fuk and det paths

--- config/settings/base.py
+++ config/settings/base.py
@@ -566,8 +566,8 @@ OAI_ROSETTA_CURATED_PREFIXES = {
     for key, value in {
         "hmt": env("OAI_ROSETTA_CURATED_PREFIX_HMT", default="/rosetta/hfmt/sandbox/input/arkumu/daten"),
         "khm": env("OAI_ROSETTA_CURATED_PREFIX_KHM", default="/rosetta/khm/sandbox/input/arkumu/daten"),
-        "fuk": env("OAI_ROSETTA_CURATED_PREFIX_FUK", default="/rosetta/fuk/sandbox/input/arkumu/daten"),
-        "det": env("OAI_ROSETTA_CURATED_PREFIX_DET", default="/rosetta/det/sandbox/input/arkumu/daten"),
+        "fuk": env("OAI_ROSETTA_CURATED_PREFIX_FUK", default="/rosetta/fudk/sandbox/input/arkumu/daten"),
+        "det": env("OAI_ROSETTA_CURATED_PREFIX_DET", default="/rosetta/hfmdt/sandbox/input/arkumu/daten"),
         "rsh": env("OAI_ROSETTA_CURATED_PREFIX_RSH", default="/rosetta/rsh/sandbox/input/arkumu/daten"),
     }.items()
     if value and value.strip()
